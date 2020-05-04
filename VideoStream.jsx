fb5d8aa37c23a97d8be01366ae5ad513155f9d92
mitrafantos <mitrafantos@gmail.com>
2020-05-04 11:12:08 +0300

    creater videoStream component

---
+++ VideoStream.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+export default class TeacherRTC extends Component {
+  constructor() {
+    super();
+    this.state = {
+      localStream: {},
+    };
+  }
+
+  componentDidMount() {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true, video: {
+        width: { min: 160, ideal: 640, max: 1280 },
+        height: { min: 120, ideal: 360, max: 720 },
+      }, })
+      .then((stream) => (this.localVideo.srcObject = stream));
+  }
+
+  render() {
+    return <video autoPlay muted ref={(video) => (this.localVideo = video)} />;
+  }
+}
