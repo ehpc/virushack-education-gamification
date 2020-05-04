00d04924cb352d420ec608b1791abad1d49a122f
Tapac <tg@elbrusboot.camp>
2020-05-04 17:24:24 +0300

    try/catch

--- fire/fair.js
+++ fire/fair.js
@@ -28,17 +28,12 @@
 		ref.orderByKey().endAt("Антонина Ивановна").on("child_added", function(snapshot) {
 		  console.log(snapshot.key, 'подключен');
 		});
-/*		
-		//testListener
-		uchilkaDB.ref().on("child_added", function(snapshot) {
-		  console.log(snapshot.key, 'эээээээ уасяыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыы');
-		});
-*/
 
 
 		// ЗАПИСЬ НОВОГО ЮЗЕРА В БД
 		function addUser(name, email, pwd, secret, coins) {
 			let teacher = false;
+			let newUser = 0; //костыль
 			if (secret == 'ya_uchilka') {
 				teacher = true;
 			};
@@ -50,47 +45,46 @@
 			}
 			userId = 'user_' + userId;
 			//////////
-			const newUser = uchilkaDB.ref('users/' + userId).set({
-				name,
-				email,
-				pwd,
-				teacher,
-				coins
-			}, function(error) {
-				if (error) {
-					console.log(newUser, 'addUser failed')
-				} else {
-					console.log(newUser, 'addUser suckcess')
-				}
-			});
+			try{
+				const newUser = uchilkaDB.ref('users/' + userId).set({
+					name,
+					email,
+					pwd,
+					teacher,
+					coins
+				});
+				console.log(newUser, 'addUser suckcess');
+			} catch (error) {
+				console.log('addUser failed');
+			}
 			return newUser;
 		}
 		//addUser('Maria Ivanovna', 'teacher@mail.ru', 123123, 'ya_uchilka', 9999999);
-		//addUser('Valera', 'v@mail.ru', 123123, 'secret', 500);
+		//addUser('Gennadiy', 'g@mail.ru', 123123, 'secret', 500);
 
 
 		// ОБНОВЛЕНИЕ ЮЗЕРА В БД
-		function updateUser(userId, name, email, pwd, secret, coins) {
+		async function updateUser(userId, name, email, pwd, secret, coins) {
 			const teacher = false;
+			let updatedUser = 0; //костыль
 			if (secret == 'ya_uchilka') {
 				teacher = true;
 			};
-			const updatedUser = uchilkaDB.ref('users/' + userId).set({
-				name,
-				email,
-				pwd,
-				teacher,
-				coins
-			}, function(error) {
-				if (error) {
-					console.log(updatedUser, 'updatedUser failed');
-				} else {
-					console.log(updatedUser, 'updatedUser suckcess');
-				}
-			});
-			return updatedUser;
+			try{
+				await uchilkaDB.ref('users/' + userId).set({ //TODO: сделать update а не set
+					name,
+					email,
+					pwd,
+					teacher,
+					coins
+				});
+				console.log(userId, 'updateUser suckcess');
+			} catch (error){
+				console.log(error, 'updateUser failed');
+			}
+			return userId;
 		}
-		//updateUser('user_cxnp3doa7ozs7ot', 'Olegka Olegi4', 'ollll@mail.ru', '123123', 'secret', 150);
+		//updateUser('user_3q5shnlak6a1gyz', 'Oleg', 'ol@mail.ru', '123123', 'secret', 150);
 
 
 		// ЗАЛОГИНИТЬСЯ 
@@ -117,21 +111,22 @@
 		// ДОБАВИТЬ/ОТНЯТЬ КОИНЫ
 		async function updateUserCoins(userId, secret, coinsss) {
 			if (secret == 'ya_uchilka') {
-				const userCoins = await uchilkaDB.ref('/users/' + userId).once('value');
-				let coins = userCoins.val().coins + coinsss;
-				const updatedCoins = await uchilkaDB.ref('users/' + userId).update({
-					coins
-				}, function(error) {
-					if (error) {
-						console.log('updateUserCoins failed');
-					} else {
-						console.log('updateUserCoins suckcess, ' + coins + ' now');
-					}
-				});
+				let updatedCoins = 0; //костыль
+				try{
+					const userCoins = await uchilkaDB.ref('/users/' + userId).once('value');
+					let coins = userCoins.val().coins + coinsss;
+					updatedCoins = await uchilkaDB.ref('users/' + userId).update({
+						coins
+					});
+					console.log('updateUserCoins suckcess, ' +userId+ ' have ' + coins + ' now');
+				} catch(error) {
+					console.log(error, 'updateUserStatus failed');
+				}
 				return updatedCoins;
 			}
 		}	
-		//updateUserCoins('user_cxnp3doa7ozs7ot', 'ya_uchilka', 500);
+		//updateUserCoins('user_3q5shnlak6a1gyz', 'ya_uchilka', 5000); 
+
 
 		// ИЗМЕНИТЬ СТАТУС
 		async function updateUserStatus(userId, secret, status) {
@@ -143,100 +138,78 @@
 				userStatus[now] = status;
 				const latestStatus = Math.max.apply(Math, Object.keys(userStatus));
 				const latestUserStatus = JSON.stringify({[userId] : userStatus[latestStatus]});
-				const updatedStatus = await uchilkaDB.ref('users/' + userId).update({
-					status:userStatus
-				}, function(error) {
-					if (error) {
-						console.log('updateUserCoins failed');
-					} else {
-						console.log('updateUserCoins suckcess, ' + latestUserStatus + ' now');
-					}
-				});
+				try{
+					const updatedStatus = await uchilkaDB.ref('users/' + userId).update({
+						status:userStatus
+					});
+					console.log('updateUserStatus suckcess, ' + latestUserStatus + ' now');
+				} catch (error){
+					console.log(error, 'updateUserStatus failed');
+				}
 				return latestUserStatus;
 			}
 		}	
-		//updateUserStatus('user_3jn5k6df9ajdevx', 'ya_uchilka', 'снова молодец');
+		//updateUserStatus('user_3q5shnlak6a1gyz', 'ya_uchilka', 'прогульщик'); 
 
 
 		// ИЗМЕНИТЬ СОСТОЯНИЕ
 		async function updateUserState(userId, state) {
-				//const userState = await uchilkaDB.ref('/users/' + userId).once('state');
-				//let coins = userState.val().coins + coinsss;
-				const now = Math.floor(Date.now() / 1000);
-				const userStateQ = await uchilkaDB.ref('/users/' + userId).once('value');
-				let userState = userStateQ.val().state;
-				if (!userState || userState.length < 3) {userState = {}};
-				userState[now] = state;
-				const latestState = Math.max.apply(Math, Object.keys(userState));
-				const latestUserState = JSON.stringify({[userId] : userState[latestState]});
-				//console.log(JSON.stringify({[userId] : userState[latestState]}), 'ssssssssssssssssssssstate')
+			const now = Math.floor(Date.now() / 1000);
+			const userStateQ = await uchilkaDB.ref('/users/' + userId).once('value');
+			let userState = userStateQ.val().state;
+			if (!userState || userState.length < 3) {userState = {}};
+			userState[now] = state;
+			const latestState = Math.max.apply(Math, Object.keys(userState));
+			const latestUserState = JSON.stringify({[userId] : userState[latestState]});
+			try{
 				const updatedState = await uchilkaDB.ref('users/' + userId).update({
 					state:userState
-				}, function(error) {
-					if (error) {
-						console.log('updateUserState failed');
-					} else {
-						console.log('updateUserState suckcess, ' + latestUserState + ' now');
-					}
 				});
-				//const eee = JSON.stringify({[userId] : [state]});
-				//console.log(eee)
+				console.log('updateUserState suckcess, ' + latestUserState + ' now');
+			} catch (error){
+				console.log(error, 'updateUserState failed');
+			}
 				return latestUserState;
 		}	
-		//updateUserState('user_3jn5k6df9ajdevx', 'поднял руку');
+		//updateUserState('user_3q5shnlak6a1gyz', 'ответил ДА');
 
 
 		// ОТОЙТИ С ПРЕДУПРЕЖДЕНИЕМ
 		async function updateUserAway(userId) {
-				const now = Math.floor(Date.now() / 1000);
-				const updatedUserAway = JSON.stringify({[userId] : [now]});
-				updateUserState(userId, 'отошел с предупреждением');
-				setTimeout(updateUserState, 10000, 'отошел без предупреждения');
+			const now = Math.floor(Date.now() / 1000);
+			const updatedUserAway = JSON.stringify({[userId] : [now]});
+			updateUserState(userId, 'отошел с предупреждением');
+			setTimeout(updateUserState, 10000, 'отошел без предупреждения');
+			try{
 				const updatedAway = await uchilkaDB.ref('users/' + userId).update({
 					away_from:now
-				}, function(error) {
-					if (error) {
-						console.log('updateUserAway failed');
-					} else {
-						console.log('updateUserAway suckcess, ' + updatedUserAway + ' now');
-					}
 				});
-				
-				console.log(updatedUserAway);
-				return updatedUserAway;
+				console.log('updateUserAway suckcess, ' + updatedUserAway + ' now');
+			} catch(error){
+				console.log(error, 'updateUserAway failed');
+			}
+			return updatedUserAway;
 		}	
 		//updateUserAway('user_3q5shnlak6a1gyz');
 
+
 		// ВЕРНУТЬСЯ
 		async function updateUserArrive(userId) {
-				const now = Math.floor(Date.now() / 1000);
-				const updatedUserArrive = JSON.stringify({[userId] : [now]});
-				updateUserState(userId, 'на месте');
+			const now = Math.floor(Date.now() / 1000);
+			const updatedUserArrive = JSON.stringify({[userId] : [now]});
+			updateUserState(userId, 'на месте');
+			try{
 				const updatedArrive = await uchilkaDB.ref('users/' + userId).update({
 					away_from:null
-				}, function(error) {
-					if (error) {
-						console.log('updateUserArrive failed');
-					} else {
-						console.log('updateUserArrive suckcess, ' + updatedUserArrive + ' now');
-					}
 				});
-				//console.log(updatedUserArrive);
-				return updatedUserArrive;
+				console.log('updateUserArrive suckcess, ' + updatedUserArrive + ' now');
+			} catch(error) {
+				console.log(error, 'updateUserArrive failed');
+			}
+			//console.log(updatedUserArrive);
+			return updatedUserArrive;
 		}	
 		//updateUserArrive('user_3q5shnlak6a1gyz');
-
-		function readUsers(){
-		const userId = 'user_cxnp3doa7ozs7ot';
-		const users = uchilkaDB.ref('/users/' + userId).once('value').then(function(snapshot) {
-		  //var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-		  console.log(snapshot.val().coins,'ASHOT!')
-		  
-		});
-		console.log(users,'users--------------')
-		}
-		//readUsers();
-
 
 	};
 	//fire();
